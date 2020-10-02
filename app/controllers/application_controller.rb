f95084bce5cf58556ec3e6a1a7d6298260c58a76
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2020-10-02 17:56:49 +0900

    新規ユーザー登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nicname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:encrypted_password,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth_date,] )
   end
 
   def basic_auth
