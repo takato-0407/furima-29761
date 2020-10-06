576289dc1b6f5acf4857236c2c79c5bd71b8f0c1
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2020-10-06 20:26:56 +0900

    ActiveHashのデータ作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   
   validates :name, presence: true
   validates :info, presence: true
-  validates :name, presence: true
+  validates :price, :format {with://}
   validates :scheduled_delivery_id, presence: true
   validates :shipping_fee_status_id, presence: true
   validates :prefecture_id, presence: true
