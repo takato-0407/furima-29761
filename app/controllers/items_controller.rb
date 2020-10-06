60ab358673bd0bccdcce91bd68980ae6f2640a4c
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2020-10-06 21:27:33 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,24 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @articles = item.all("created_at DESC")
   end
 
   def new
-    @item = Item.new
+    @articles = item.new
   end 
+  def create
+    @article = item.new(item_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:article).permit(:scheduled_delivery,:category,:prefecture,:shipping_fee_status,:sales_status,)
+  end
 end
