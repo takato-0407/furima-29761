d58e4cfdc516716a821d783360372a30b6564e5b
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2020-10-08 14:26:05 +0900

    価格範囲の設定

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -8,12 +8,19 @@ function pullDown() {
 
   pullDownButton.addEventListener('keyup', function(){
     let price = pullDownButton.value;
-    let fee = price * 0.1
-    let gains = price - fee
-    pullDownParents.innerHTML = fee
-    pullDownChild.innerHTML = gains
+    if (300 <= price && price <= 9999999){
+      let fee = Math.floor(price * 0.1)
+      let gains = price - fee
+      pullDownParents.innerHTML = fee
+      pullDownChild.innerHTML = gains
+    } else {
+      pullDownParents.innerHTML = '-'
+      pullDownChild.innerHTML = '-'
+    }
     })
   }
 
 
 window.addEventListener('load', pullDown)
+
+
