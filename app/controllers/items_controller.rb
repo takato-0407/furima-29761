c65ebd1c879e9730f899b3db866765e1e57183ab
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2020-10-12 16:15:05 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
    @items = Item.all.order(created_at: :DESC)
@@ -26,7 +26,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.valid?
       redirect_to item_path
