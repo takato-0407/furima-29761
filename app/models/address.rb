9b4997d588b2ee8404435588d6c1d94035e71a02
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2020-10-13 11:24:43 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ApplicationRecord
+  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+  validates :postal_code, presence: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :building, presence: true
+  validates :phone_number, format: { with: /{10.11\d}/}
 end
